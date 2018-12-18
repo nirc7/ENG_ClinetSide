63d2ec66e61e2d4d4854da54a9c435df4b161dd1
Lab4 RUPPIN <lab4@ruppin.ac.il>
2018-12-18 12:28:38 +0200

    demo2

---
+++ Lecture 3 - JS/JsDemoProj/WebApplication1/myScripts.js
@@ -0,0 +1,11 @@
+﻿
+function init() {
+    alert('init');
+}
+
+function func1() {
+    var num = 7; //local variable
+    num2 = 8;// global variable!
+    alert("hello world!" + num);  
+    alert("hello world!" + num4);  
+}
